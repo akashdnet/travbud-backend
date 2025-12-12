dfac5a72ff886ace2098c85a8ce9f4229a474a83
akashdnet <khondokerakashmi@gmail.com>
2025-12-12 16:33:14 +0600

    new file upload file 2

--- src/modules/user/user.route.ts
+++ src/modules/user/user.route.ts
@@ -1,7 +1,6 @@
 import { Router } from "express";
 import authGuard from "../../middlewares/auth";
 import { fileUploader } from "../../middlewares/fileUploader";
-import { upload } from "../../middlewares/upload";
 import { validateRequest } from "../../utils/validationRequest";
 import { userController } from "./user.controller";
 import { userValidation } from "./user.validation";
@@ -16,14 +15,14 @@ const router = Router();
 // user routes 
 router.post("/register", fileUploader.upload.single('image'), validateRequest(userValidation.userRegistrationValidation), userController.createUser);
 router.get("/me", authGuard("admin", "user"), userController.getSingleUser);
-router.patch("/update/me", upload.single("image"), validateRequest(userValidation.userUpdateValidation), authGuard("admin", "user"), userController.updateUser);
+router.patch("/update/me", fileUploader.upload.single('image'), validateRequest(userValidation.userUpdateValidation), authGuard("admin", "user"), userController.updateUser);
 router.delete("/me", authGuard("user"), userController.deleteUser);
 
 
 // admin routes 
 router.get("/all-users", authGuard("admin"), userController.getAllUsers);
 router.get("/:id", userController.getSingleUser);
-router.patch("/update/:id", upload.single("image"), validateRequest(userValidation.adminUserUpdateValidation), authGuard("admin"), userController.updateUser);
+router.patch("/update/:id", fileUploader.upload.single('image'), validateRequest(userValidation.adminUserUpdateValidation), authGuard("admin"), userController.updateUser);
 router.delete("/:id", authGuard("admin"), userController.deleteUser);
 
 
